6c21aca7f194177d5f1c551a49eca517e9de6efb
ajihermansya <ajiheransya14@gmail.com>
2024-03-02 16:32:18 +0700

    done navigation

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
 import TabNavigator from './src/navigators/TabNavigator';
 import DetailsScreen from './src/screens/DetailsScreen';
 import PaymentScreen from './src/screens/PaymentScreen';
