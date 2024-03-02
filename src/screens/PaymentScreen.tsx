10d878dadfba5c6e667d32af1730b9e46c8b582b
ajihermansya <ajiheransya14@gmail.com>
2024-03-02 13:15:08 +0700

    add to navigation

---
+++ src/screens/PaymentScreen.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const PaymentScreen = () => {
+  return (
+    <View>
+      <Text>PaymentScreen</Text>
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({})
+
+
+export default PaymentScreen
